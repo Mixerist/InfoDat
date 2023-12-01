1e2745c6631547d154a0172b31bd3506bfd1d4a7
Mixerist <61286875+Mixerist@users.noreply.github.com>
2023-12-01 19:26:19 +0300

    Update Program.cs

--- InfoDat/Program.cs
+++ InfoDat/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace InfoDat
+﻿using System.Globalization;
+
+namespace InfoDat
 {
     internal static class Program
     {
         private static void Main(string[] args)
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
             new Parser().Run();
         }
     }
